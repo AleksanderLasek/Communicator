83f3e773e74cbb4db2451e1ca8e168399591d3c9
Aleksander Lasek <aleksanderlasek01@gmail.com>
2023-03-09 13:35:51 +0100

    added routing

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,130 +1,175 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import * as S from './index.styles';
-import axios from 'axios';
+import * as S from "./index.styles";
+import axios from "axios";
 import { useCookies } from "react-cookie";
-
+import Router from "../Router";
 
 const App = () => {
-    const [swap, setSwap] = useState(true);
-    const [logged, setLogged] = useState(false);
-    const [choice, setChoice] = useState(false);
-    const [cookie, setCookie, removeCookie] = useCookies(['refreshToken']);
-    const [inputBorder, setInputBorders] = useState('');
-    const [error, setError] = useState('');
-    const [user, setUser] = useState({
-        name: "",
-        surname: "",
-        email: "",
-        password: ""
-    })
-    const handleUser = (e) => {
-        setUser(prev => ({...prev, [e.target.name]: e.target.value}));
+  const [swap, setSwap] = useState(true);
+  const [logged, setLogged] = useState(false);
+  const [choice, setChoice] = useState(false);
+  const [cookie, setCookie, removeCookie] = useCookies(["refreshToken"]);
+  const [inputBorder, setInputBorders] = useState("");
+  const [error, setError] = useState("");
+  const [user, setUser] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  });
+  const handleUser = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  const pageTheme = (data) => {
+    setSwap(data);
+  };
+  const handleChoice = () => {
+    setChoice((current) => !current);
+    setInputBorders("");
+  };
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/users/login", user);
+
+      if (res.status === 200) {
+        const expire = new Date();
+        expire.setTime(expire.getTime() + 60 * 60 * 24 * 1000);
+        console.log(expire);
+        setCookie("refreshToken", res.data.refreshToken, {
+          path: "/",
+          expires: expire,
+        });
+      } else if (res.status === 201) {
+        setInputBorders("1px solid red");
+      } else {
+        setError(res.data.msg);
+      }
+      console.log(res);
+    } catch (error) {
+      console.log(error);
     }
-    const pageTheme = (data) => {
-        setSwap(data);
+  };
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/users", user);
+      if (res.status === 200) {
+        setError("");
+        handleSignIn(e);
+      } else if (res.status === 201) {
+        setInputBorders("1px solid red");
+      } else {
+        setError(res.data.msg);
+      }
+    } catch (err) {
+      console.log(err);
     }
-    const handleChoice = () => {
-        setChoice(current => !current);
-        setInputBorders('');
+  };
+  const handleEnter = (event) => {
+    if (event.key === "Enter") {
+      handleSignIn(event);
     }
-    const handleSignIn = async e => {
-        e.preventDefault();
-        try {
-            const res = await axios.post("http://localhost:5000/users/login", user);
-            
-            if(res.status === 200) {
-                const expire = new Date();
-                expire.setTime(expire.getTime() + 60 * 60 * 24 * 1000);
-                console.log(expire)
-                setCookie('refreshToken', res.data.refreshToken, {path: "/", expires: expire});
-            }else if(res.status === 201) {
-                setInputBorders('1px solid red');
-            }else{
-                setError(res.data.msg);
-            }
-            console.log(res);
-        }catch(error){
-            console.log(error);
-        }
+  };
+  const handleEnterOnSignUp = (event) => {
+    if (event.key === "Enter") {
+      handleSignUp(event);
     }
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post("http://localhost:5000/users", user);
-            if(res.status === 200) {
-                setError('');
-                handleSignIn(e);
-            }else if(res.status === 201) {
-                setInputBorders('1px solid red');
-            }else{
-                setError(res.data.msg);
-            }
-           
-        }catch(err){
-            console.log(err);
-        }
-    }
-    const handleEnter = (event) => {
-        if(event.key === 'Enter') {
-            handleSignIn(event);
-        }
-    }
-    const handleEnterOnSignUp = (event) => {
-        if(event.key === 'Enter') {
-            handleSignUp(event);
-        }
-    }
-    return (
-        <>
-        <Header pageTheme={pageTheme}/>
-        <S.Wrapper pageTheme={swap}>
-            
-            {cookie.refreshToken ? (
-                <>
-                    
-                </>
+  };
+  return (
+    <>
+      <Header pageTheme={pageTheme} />
+      <S.Wrapper pageTheme={swap}>
+        <Router path="/chat">jakis napis</Router>
+        <Router path={"/friends"}>friends</Router>
+        <Router path={"/nots"}>nots</Router>
+        {cookie.refreshToken ? (
+          <></>
+        ) : (
+          <>
+            {choice ? (
+              <S.LoginWrapper pageTheme={swap}>
+                <S.WelcomeMessage>Welcome!</S.WelcomeMessage>
+                <S.InputsWrapper>
+                  <S.Input
+                    placeholder="Email"
+                    name="email"
+                    onChange={handleUser}
+                    style={{ border: inputBorder }}
+                    onKeyPress={handleEnter}
+                  />
+                  <S.Input
+                    placeholder="Password"
+                    type="password"
+                    name="password"
+                    onChange={handleUser}
+                    style={{ border: inputBorder }}
+                    onKeyPress={handleEnter}
+                  />
+                  <S.Button onClick={handleSignIn}>Sign in</S.Button>
+                  {error}
+                </S.InputsWrapper>
+                <S.WelcomeMessage>
+                  Don't have an account?
+                  <br />
+                  <S.ChangeButton onClick={handleChoice}>
+                    Sign up
+                  </S.ChangeButton>
+                </S.WelcomeMessage>
+              </S.LoginWrapper>
             ) : (
-                <>
-                {choice ? (
+              <>
                 <S.LoginWrapper pageTheme={swap}>
-                    <S.WelcomeMessage>Welcome!</S.WelcomeMessage>
-                    <S.InputsWrapper>
-                        <S.Input placeholder="Email" name="email" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnter}/>
-                        <S.Input placeholder="Password" type="password" name="password" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnter}/>
-                        <S.Button onClick={handleSignIn}>Sign in</S.Button>
-                        {error}
-                    </S.InputsWrapper>
-                    <S.WelcomeMessage>
-                        Don't have an account?<br/>
-                        <S.ChangeButton onClick={handleChoice}>Sign up</S.ChangeButton>
-                    </S.WelcomeMessage>
+                  <S.WelcomeMessage>Welcome!</S.WelcomeMessage>
+                  <S.InputsWrapper>
+                    <S.Input
+                      placeholder="Name"
+                      name="name"
+                      onChange={handleUser}
+                      style={{ border: inputBorder }}
+                      onKeyPress={handleEnterOnSignUp}
+                    />
+                    <S.Input
+                      placeholder="Surname"
+                      name="surname"
+                      onChange={handleUser}
+                      style={{ border: inputBorder }}
+                      onKeyPress={handleEnterOnSignUp}
+                    />
+                    <S.Input
+                      placeholder="Email"
+                      name="email"
+                      onChange={handleUser}
+                      style={{ border: inputBorder }}
+                      onKeyPress={handleEnterOnSignUp}
+                    />
+                    <S.Input
+                      placeholder="Password"
+                      type="password"
+                      name="password"
+                      onChange={handleUser}
+                      style={{ border: inputBorder }}
+                      onKeyPress={handleEnterOnSignUp}
+                    />
+                    <S.Button onClick={handleSignUp}>Sign up</S.Button>
+                    {error}
+                  </S.InputsWrapper>
+                  <S.WelcomeMessage>
+                    Already have an account?
+                    <br />
+                    <S.ChangeButton onClick={handleChoice}>
+                      Sign in
+                    </S.ChangeButton>
+                  </S.WelcomeMessage>
                 </S.LoginWrapper>
-                
-                ) : (
-                    <>
-                    <S.LoginWrapper pageTheme={swap}>
-                    <S.WelcomeMessage>Welcome!</S.WelcomeMessage>
-                    <S.InputsWrapper>
-                        <S.Input placeholder="Name" name="name" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnterOnSignUp}/>
-                        <S.Input placeholder="Surname" name="surname" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnterOnSignUp}/>
-                        <S.Input placeholder="Email" name="email" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnterOnSignUp}/>
-                        <S.Input placeholder="Password" type="password" name="password" onChange={handleUser} style={{border: inputBorder}} onKeyPress={handleEnterOnSignUp}/>
-                        <S.Button onClick={handleSignUp}>Sign up</S.Button>
-                        {error}
-                    </S.InputsWrapper>
-                    <S.WelcomeMessage>
-                        Already have an account?<br/>
-                        <S.ChangeButton onClick={handleChoice}>Sign in</S.ChangeButton>
-                    </S.WelcomeMessage>
-                </S.LoginWrapper>
-                    </>
-                )}
-                </>
+              </>
             )}
-        </S.Wrapper>
-        </>
-    )
-}
+          </>
+        )}
+      </S.Wrapper>
+    </>
+  );
+};
 
 export default App;
