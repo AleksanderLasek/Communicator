c244c5de7c3b14d02cf0714743be74c134980bda
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-05-08 22:50:18 +0200

    drive update

--- backend/controllers/Files.js
+++ backend/controllers/Files.js
@@ -1,5 +1,7 @@
-const credentials = require('../models/credentials.json')
-const { google } = require('googleapis')
+import  credentials  from '../models/credentials.json' assert { type: "json" };
+import { google } from 'googleapis';
+import fs from 'fs'
+
 
 const client = new google.auth.JWT(
   credentials.client_email,
@@ -9,113 +11,39 @@ const client = new google.auth.JWT(
 );
 const drive = google.drive({ version: 'v3', auth: client });
 
-const fs = require('fs');
-
-
-const fileMetadata = {
-    name: 'nazwa_pliku',
-    mimeType: 'typ/mime'
-  };
-  
-  const media = {
-    mimeType: 'typ/mime',
-    body: fs.createReadStream('../models/credentials.json')
-  };
-  
-  drive.files.create(
-    {
-      resource: fileMetadata,
-      media: media,
-      fields: 'id'
-    },
-    function (err, file) {
-      if (err) {
-        // obsługa błędu
-        console.error(err);
-      } else {
-        console.log('File Id:', file.data.id);
-      }
-    }
-  );
-
-drive.files.list({
-  pageSize: 10,
-  fields: 'nextPageToken, files(id, name)',
-}, (err, res) => {
-  if (err) return console.error('The API returned an error:', err.message);
-  const files = res.data.files;
-  if (files.length) {
-    console.log('Files:');
-    files.forEach((file) => {
-      console.log(`${file.name} (${file.id})`);
+export const uploadFile = async (req, res) => {
+  const { file } = req;
+  console.log(req.file)
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        name: file.originalname,
+        mimeType: file.mimetype,
+      },
+      media: {
+        mimeType: file.mimetype,
+        body: fs.createReadStream(file.path),
+      },
     });
-  } else {
-    console.log('No files found.');
+    console.log(`File uploaded: ${response.data.name} (${response.data.id})`);
+    return res.status(200).send(response.data.id);
+   
+  } catch (error) {
+    return res.status(404).send({msg: 'Error'});
   }
-});
-
-
-const fs = require('fs');
-const { google } = require('googleapis');
-const credentials = require('../models/credentials.json');
-
-const client = new google.auth.JWT(
-  credentials.client_email,
-  null,
-  credentials.private_key,
-  ['https://www.googleapis.com/auth/drive']
-);
-
-const drive = google.drive({ version: 'v3', auth: client });
-
-// 1. Otwórz plik ze zdjęciem
-const file = fs.createReadStream('/path/to/image.jpg');
-
-// 2. Dodaj zdjęcie do Dysku Google
-const fileMetadata = {
-  name: 'image.jpg',
-  parents: ['folder-id'], // opcjonalnie: wskazanie folderu, do którego ma trafić plik
 };
 
-const media = {
-  mimeType: 'image/jpeg',
-  body: file,
-};
+export const GetFile = async(req, res) => {
+  const { file_id } = req.body;
 
-drive.files.create(
-  {
-    resource: fileMetadata,
-    media: media,
-    fields: 'id',
-  },
-  function (err, file) {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('File Id:', file.data.id);
-
-      // 3. Pobierz zdjęcie z Dysku Google
-      drive.files.get(
-        {
-          fileId: file.data.id,
-          alt: 'media',
-        },
-        { responseType: 'stream' },
-        function (err, res) {
-          if (err) {
-            console.error(err);
-          } else {
-            // 4. Wyświetl zdjęcie na stronie
-            res.data.pipe(fs.createWriteStream('/path/to/downloaded/image.jpg'));
-
-            // Można również przekazać strumień do biblioteki do wyświetlenia zdjęcia
-            // np. do biblioteki ExpressJS:
-            // res.setHeader('Content-Type', 'image/jpeg');
-            // res.setHeader('Content-Disposition', 'inline; filename="image.jpg"');
-            // res.status(200).send(res.data);
-          }
-        }
-      );
-    }
-  }
-);
+  drive.files.get({
+      fileId: '1txQs4KDpTWtBHuCKhrTK9SyVKGFFKcwu',
+      alt: 'media'
+  }, { responseType: 'arraybuffer' }, (err, response) => {
+      if (err) return console.error('The API returned an error:', err.message);
+      
+  const image = Buffer.from(response.data, 'binary');
+    return res.status(200).send({image});  
+  });
+  
+}
